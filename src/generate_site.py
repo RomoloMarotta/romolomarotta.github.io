86bcc548583d736f77bdb1e9f582c35530d51392
romolo.marotta@gmail.com <romolo.marotta@gmail.com>
2021-10-28 06:35:09 +0200

    Removed useless files

--- src/generate_site.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.system("rm -R public_html/*")
-os.system("cp -R components/css public_html/")
-os.system("cp -R components/images public_html/")
-os.system("cp -R components/slides public_html/")
-os.system('for i in *.xml; do python build_page.py $i; done ')
